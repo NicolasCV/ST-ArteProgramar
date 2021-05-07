59b2454213707b9a59851e04177c03e617b16e4e
Nicolas CV <niccarval@hotmail.com>
2021-05-07 10:01:10 -0600

    Nicolas

--- Act5Memorama/memorama.py
+++ Act5Memorama/memorama.py
@@ -4,8 +4,9 @@
 #Reflexiones:
 #Nicolas
 '''
-
-
+Aprendi un poco mas a utilizar matrices en un ejemplo como este
+Aprendi mas sobre la libreria turtle y como puede ser utilizada
+Practique aun mas con Github
 '''
 
 #Angel
@@ -17,8 +18,9 @@
 from random import *
 from turtle import *
 from freegames import path
+from PIL import Image
 
-car = path('car.gif')
+#Lo que va a tener nuestro memorama
 alumnos = """Tere
 Luis Carlos
 Alexandra
@@ -53,48 +55,68 @@ Andrés
 Ricardo"""
 tiles = (alumnos.split('\n'))*2
 shuffle(tiles)
+
+
 state = {'mark': None}
+
+#Matriz de estados (hidden or not)
 hide = [True] * 64
 
+tapCounter = 0
+
+#Para dibujar el tablero
 def square(x, y):
     "Draw white square with black outline at (x, y)."
     up()
     goto(x, y)
     down()
-    color('black', 'white')
+    color('black', 'whitesmoke')
     begin_fill()
     for count in range(4):
         forward(50)
         left(90)
     end_fill()
 
+#Regresa el tile en el que se le esta picando
 def index(x, y):
     "Convert (x, y) coordinates to tiles index."
     return int((x + 200) // 50 + ((y + 200) // 50) * 8)
 
+#Regresa el xy del tile que se le esta picando
 def xy(count):
     "Convert tiles count to (x, y) coordinates."
     return (count % 8) * 50 - 200, (count // 8) * 50 - 200
 
+
+#Cuando hace click
 def tap(x, y):
+    global tapCounter
     "Update mark and hidden tiles based on tap."
     spot = index(x, y)
     mark = state['mark']
+    tapCounter+=1
 
+    
     if mark is None or mark == spot or tiles[mark] != tiles[spot]:
+        #Cuando son diferentes
         state['mark'] = spot
     else:
+        #Si si son iguales ya no se esconde
         hide[spot] = False
         hide[mark] = False
         state['mark'] = None
 
+
 def draw():
     "Draw image and tiles."
+    global tapCounter
     clear()
     goto(0, 0)
-    shape(car)
+    shape(tec)
     stamp()
+    #Se coloca la imagen
 
+    #Se hace la cuadricula
     for count in range(64):
         if hide[count]:
             x, y = xy(count)
@@ -102,21 +124,48 @@ def draw():
 
     mark = state['mark']
 
+    #Se escribe lo valido
     if mark is not None and hide[mark]:
         x, y = xy(mark)
         up()
-        goto(x + 2, y)
-        color('black')
-        write(tiles[mark], font=('Arial', 30, 'normal'))
+        goto(x + 10, y+10)
+        color('blue')
+        write(tiles[mark], font=('Arial', 15, 'normal'))
+
+    #Para marcar que se gano
+    if True not in hide:
+        up()
+        goto(-80,-210)
+        color('blue')
+        write('Ganaste!', font = ('Arial',30,'normal'))
+
+    #Dibuja el numero de taps
+    up()
+    goto(0, 200)
+    color('blue')
+    write(f"Numero de taps: {tapCounter}", font=('Arial', 14, 'normal'))
+    up()
+    goto(-210,-215)
+    write('Angel Vargas A00829204 / Nicolas Cardenas A01114959',font=('Arial', 10, 'normal'))
 
     update()
+    #Se actualiza cada 100 ms
     ontimer(draw, 100)
 
-shuffle(tiles)
-setup(420, 420, 370, 0)
-addshape(car)
+#Ventana
+setup(450, 450, 370, 0)
+
+#Se carga la imagen
+tec = 'TECm.gif'
+
+#Se agrega la imagen
+addshape(tec)
+
+#Se esconde la turtle
 hideturtle()
 tracer(False)
+
+#Cada vez que hay un click se ejecuta  tap
 onscreenclick(tap)
 draw()
 done()
